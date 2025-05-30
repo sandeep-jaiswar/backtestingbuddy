01af3c35b5f0165f045bfbba80446fc6b1d65db6
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2025-05-31 01:35:22 +0530

    feat: add initial configuration files for PostCSS, Prettier, implement bundle size reporting script, add TypeScript reset and query selector types, add tsconfig.json with settings tailored for a Next.js project, including strict type checking and module resolution.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # backtestbuddy
+
 BacktestBuddy lets retail traders backtest any idea with real-world conditions, understand every trade decision, and get Telegram alerts — all using visual strategy blocks with beautiful charts
---
+++ app/page.tsx
@@ -0,0 +1,37 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "backtestbuddy | home",
+  applicationName: "backtestbuddy",
+  icons: {
+    icon: "@/assets/favicon.ico"
+  },
+  twitter: {
+    card: "summary_large_image",
+  },
+  openGraph: {
+    url: "https://backtestbuddy.com",
+    title: "Backtestbuddy",
+    images: [
+      {
+        width: 1200,
+        height: 630,
+        url: "https://raw.githubusercontent.com/sandeep-jaiswar/backtestbuddy/main/.github/assets/project-logo.png",
+      },
+    ],
+  },
+}
+
+export default function Web() {
+  return (
+    <section className="bg-white dark:bg-gray-900">
+      <div className="mx-auto grid max-w-(--breakpoint-xl) px-4 py-8 text-center lg:py-16">
+        <div className="mx-auto place-self-center">
+          BacktestBuddy is a powerful tool designed to help you backtest your trading strategies with ease. Whether
+          you're a seasoned trader or just starting out, BacktestBuddy provides the tools you need to analyze your
+          strategies and make informed decisions.
+        </div>
+      </div>
+    </section>
+  )
+}
