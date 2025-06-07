aa5cb2f797c8948d7477c02b1e6d63a32d785101
sandeep-jaiswar <jaiswarsandeep119@gmail.com>
2025-06-07 18:34:31 +0530

    chore: update dependencies and configuration for Tailwind CSS
    - Removed unused dependencies: @vercel/otel, class-variance-authority, and several @opentelemetry packages.
    - Updated tailwindcss to version 3.4.0 and typescript to version 5.3.3.
    - Modified tailwind.css to use the new @tailwind directives for base, components, and utilities.
    - Added a new Tailwind CSS configuration file (tailwind.config.ts) to specify content paths and extend theme options.

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export const metadata: Metadata = {
   title: "backtestbuddy | home",
   applicationName: "backtestbuddy",
   icons: {
-    icon: "@/assets/favicon.ico"
+    icon: "@/assets/favicon.ico",
   },
   twitter: {
     card: "summary_large_image",
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
 export default function Web() {
   return (
     <section className="bg-white dark:bg-gray-900">
-      <div className="mx-auto grid max-w-(--breakpoint-xl) px-4 py-8 text-center lg:py-16">
+      <div className="max-w-(--breakpoint-xl) mx-auto grid px-4 py-8 text-center lg:py-16">
         <div className="mx-auto place-self-center">
           BacktestBuddy is a powerful tool designed to help you backtest your trading strategies with ease. Whether
           you're a seasoned trader or just starting out, BacktestBuddy provides the tools you need to analyze your
